393c9b4163b787038e0b755c331a048e400ba4d8
unknown <michele@specsyst.com.br>
2021-08-28 16:32:29 -0300

    Fix: Corrigir dados models, nova migração e inserir models no admin.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import TipoDeProfissional, Profissional
 
-
-admin.site.register(TipoDeProfissional)
-admin.site.register(Profissional)
-
+# Register your models here.
