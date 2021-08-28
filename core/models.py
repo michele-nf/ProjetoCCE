b21fdeff42f61b1d5c83b506abf4901d735a3064
unknown <michele@specsyst.com.br>
2021-08-28 15:16:55 -0300

    Models: Criar dados, migração e tabelas no banco de dados.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.fields import CharField, EmailField
 
-# Create your models here.
+
+class TipoDeProfissional(models.Model):
+    #Dados do tipo de profissional
+    id = models.BigAutoField(primary_key=True)
+    descricao = models.CharField(max_length=50)
+    situacao = models.BooleanField(default=False)
+    updatedAt = models.DateTimeField()
+    createdAt = models.DateTimeField(editable=False)
+
+
+class Profissional(models.Model):
+    #Dados do profissional
+    id = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    tipoDeProfissional = models.ForeignKey(TipoDeProfissional, on_delete=models.CASCADE) 
+    situacao = models.BooleanField(default=False)
+    updatedAt = models.DateTimeField()
+    createdAt = models.DateTimeField(editable=False)
