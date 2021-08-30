913120613f96b1f1e57d8050b771649c7348759d
unknown <michele@specsyst.com.br>
2021-08-30 10:02:24 -0300

    Style: Atualizar botão e título.

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path(r'profissional-novo/', profissional_novo, name='core_profissional_novo'),
     #Profissional - Update
     #core/profissional-update
-    path(r'profissional-update/(?P<id>\d+)/$', profissional_update, name='core_profissional_update'),
+    path(r'^profissional-update/(?P<id>\d+)/$', profissional_update, name='core_profissional_update'),
     #Tipo de profissional - Lista
     #core/tipo_de_profissional
     path(r'tipo_de_profissional/', lista_tipo_de_profissional, 
@@ -30,5 +30,5 @@ urlpatterns = [
     path(r'tipo_de_profissional-novo/', tipo_de_profissional_novo, name='core_tipo_de_profissional_novo'),
     #Tipo de Profissional - Update
     #core/tipo_de_profissional-update
-    path(r'tipo_de_profissional-update/(?P<id>\d+)/$', tipo_de_profissional_update, name='core_tipo_de_profissional_update'),
+    path(r'^tipo_de_profissional-update/(?P<id>\d+)/$', tipo_de_profissional_update, name='core_tipo_de_profissional_update'),
 ]
